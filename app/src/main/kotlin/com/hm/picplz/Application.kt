08dfc6e0879c711bc9b2af5bb63a92f94d13bf45
ldhldh07 <dhlim07@naver.com>
2024-11-05 16:26:19 +0900

    :construction_worker: : 카카오 sdk init

--- app/src/main/kotlin/com/hm/picplz/Application.kt
+++ app/src/main/kotlin/com/hm/picplz/Application.kt
@@ -1,11 +1,18 @@
 package com.hm.picplz
 
 import android.app.Application
+import android.util.Log
+import com.kakao.vectormap.KakaoMapSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        try {
+            KakaoMapSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+        } catch (e: Exception) {
+            Log.e("KakaoMapSdk", "카카오맵 SDK init 실패", e)
+        }
     }
 }
