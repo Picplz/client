3fbd5a6ae7d6790a66aeffcdbfc70848eb93d478
ldhldh07 <dhlim07@naver.com>
2024-11-20 21:28:06 +0900

    :lipstick: : 주소 출력 프로토타입

--- app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/SearchPhotographerScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/SearchPhotographerScreen.kt
@@ -2,14 +2,18 @@ package com.hm.picplz.ui.screen.search_photographer
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -26,6 +30,9 @@ fun SearchPhotographerScreen(
     mainNavController: NavHostController,
 ) {
     val context = LocalContext.current
+
+    val currentState = viewModel.state.collectAsState().value
+
     Scaffold (
         modifier = Modifier
             .fillMaxSize(),
@@ -40,12 +47,23 @@ fun SearchPhotographerScreen(
                 text = "내 주변 작가 찾기",
                 onClickBack = {}
             )
-            KakaoMapView(
-                onMapReady = { kakaoMap ->
-                    viewModel.displayLabelsOnMap(kakaoMap)
-                    viewModel.displayAddressOnMap(kakaoMap, context)
-                }
-            )
+            Box(
+                modifier = Modifier.weight(1f)
+            ) {
+                KakaoMapView(
+                    onMapReady = { kakaoMap ->
+                        viewModel.displayLabelsOnMap(kakaoMap)
+                        viewModel.handleIntent(SearchPhotographerIntent.GetAddress(37.406960, 127.115587))
+                    }
+                )
+                Text(
+                    text = currentState.address ?: "",
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .padding(top = 8.dp),
+                    color = MainThemeColor.Black,
+                )
+            }
         }
 
     }
