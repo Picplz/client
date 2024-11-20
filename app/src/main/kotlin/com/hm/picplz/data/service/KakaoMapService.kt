f05748c8a10783711e2bd9453411e1573b4ff063
ldhldh07 <dhlim07@naver.com>
2024-11-20 20:59:12 +0900

    :construction_worker: : 카카오맵 Service 정의 - 엔드포인트 및 요청 형식 정의

---
+++ app/src/main/kotlin/com/hm/picplz/data/service/KakaoMapService.kt
@@ -0,0 +1,15 @@
+package com.hm.picplz.data.service
+
+import com.hm.picplz.data.model.KaKaoAddressResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface KakaoMapService {
+    @GET("v2/local/geo/coord2address.json")
+    suspend fun getAddressFromCoords(
+        @Header("Authorization") authorization: String,
+        @Query("x") longitude: Double,
+        @Query("y") latitude: Double
+    ): KaKaoAddressResponse
+}
