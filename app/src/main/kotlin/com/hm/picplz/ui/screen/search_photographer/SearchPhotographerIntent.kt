32b4188874223a85946d7de477d0f40ff8d8bf55
ldhldh07 <dhlim07@naver.com>
2024-11-20 21:27:13 +0900

    :construction_worker: : 주소 api 호출 intent 추가

--- app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/SearchPhotographerIntent.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/SearchPhotographerIntent.kt
@@ -2,4 +2,5 @@ package com.hm.picplz.ui.screen.search_photographer
 
 sealed class SearchPhotographerIntent {
     data class SetAddress(val address: String) : SearchPhotographerIntent()
+    data class GetAddress(val latitude: Double, val longitude: Double) : SearchPhotographerIntent()
 }
