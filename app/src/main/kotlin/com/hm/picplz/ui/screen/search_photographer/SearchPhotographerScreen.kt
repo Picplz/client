4ea17b874499300fe22acd09ba45ae2b41630935
ldhldh07 <dhlim07@naver.com>
2024-11-21 16:28:49 +0900

    :lipstick: : 주소 표시 위치 조정

--- app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/SearchPhotographerScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/SearchPhotographerScreen.kt
@@ -6,10 +6,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -60,13 +64,23 @@ fun SearchPhotographerScreen(
                         viewModel.handleIntent(SearchPhotographerIntent.SetCenterCoords(cameraPosition.position))
                     }
                 )
-                Text(
-                    text = currentState.address ?: "",
+                Surface(
                     modifier = Modifier
-                        .padding(16.dp)
-                        .padding(top = 8.dp),
-                    color = MainThemeColor.Black,
-                )
+                        .align(Alignment.BottomCenter)
+                        .padding(bottom = 16.dp),
+                    shape = RoundedCornerShape(8.dp),
+                    color = MainThemeColor.White,
+                ) {
+                    Text(
+                        text = currentState.address ?: "",
+                        modifier = Modifier.padding(
+                            horizontal = 16.dp,
+                            vertical = 8.dp
+                        ),
+                        color = MainThemeColor.Black,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
             }
         }
 
