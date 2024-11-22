c7d1820c321102ffc1cd8d6d479ead3daf50bcd1
ldhldh07 <dhlim07@naver.com>
2024-11-22 09:45:06 +0900

    :construction_worker: : 현재 위치를 저장하는 state와 set intent 함수 정의

--- app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/SearchPhotographerIntent.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/SearchPhotographerIntent.kt
@@ -1,9 +1,12 @@
 package com.hm.picplz.ui.screen.search_photographer
 
+import android.content.Context
 import com.kakao.vectormap.LatLng
 
 sealed class SearchPhotographerIntent {
     data class SetAddress(val address: String) : SearchPhotographerIntent()
     data class GetAddress(val Coords: LatLng) : SearchPhotographerIntent()
     data class SetCenterCoords(val centerCoords: LatLng) : SearchPhotographerIntent()
+    data class SetCurrentLocation(val location: LatLng) : SearchPhotographerIntent()
+    data class GetCurrentLocation(val context: Context) : SearchPhotographerIntent()
 }
--- app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/SearchPhotographerState.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/SearchPhotographerState.kt
@@ -4,13 +4,15 @@ import com.kakao.vectormap.LatLng
 
 data class SearchPhotographerState (
     val address: String? = null,
-    val centerCoords: LatLng = LatLng.from(37.406960, 127.115587)
+    val centerCoords: LatLng = LatLng.from(37.406960, 127.115587),
+    val userLocation: LatLng? = null
 ) {
     companion object {
         fun idle(): SearchPhotographerState {
             return SearchPhotographerState(
                 address = null,
-                centerCoords = LatLng.from(37.406960, 127.115587)
+                centerCoords = LatLng.from(37.406960, 127.115587),
+                userLocation = null
             )
         }
     }
