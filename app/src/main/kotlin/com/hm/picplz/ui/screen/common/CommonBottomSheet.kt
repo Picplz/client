b76385928825abf95d361510b81c5e9c1c3b405a
ldhldh07 <dhlim07@naver.com>
2024-12-12 17:40:40 +0900

    :lipstick: : 공통 bottom sheet 컴포넌트 추가

---
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonBottomSheet.kt
@@ -0,0 +1,35 @@
+package com.hm.picplz.ui.screen.common
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun CommonBottomSheetScaffold(
+    modifier: Modifier = Modifier,
+    sheetContent: @Composable ColumnScope.() -> Unit,
+    sheetPeekHeight: Float = 64f,
+    sheetShape: Shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+    content: @Composable (PaddingValues) -> Unit,
+) {
+    val scaffoldState = rememberBottomSheetScaffoldState()
+    
+    BottomSheetScaffold(
+        modifier = modifier,
+        scaffoldState = scaffoldState,
+        sheetContent = {
+            sheetContent()
+        },
+        sheetPeekHeight = sheetPeekHeight.dp,
+        sheetShape = sheetShape,
+        content =  content,
+    )
+}
