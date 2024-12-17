1bcaebb44bb0e5850c4acab2fed30cba1c45bb11
ldhldh07 <dhlim07@naver.com>
2024-12-17 15:09:37 +0900

    :sparkles: : 공통 bottom sheet 수정 - 다시 BottomSheetScaffold로 수정

--- app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonBottomSheet.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonBottomSheet.kt
@@ -1,35 +1,80 @@
 package com.hm.picplz.ui.screen.common
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.hm.picplz.ui.theme.MainThemeColor
+
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun CommonBottomSheetScaffold(
     modifier: Modifier = Modifier,
     sheetContent: @Composable ColumnScope.() -> Unit,
-    sheetPeekHeight: Float = 64f,
-    sheetShape: Shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+    sheetPeekHeight: Dp = 30.dp,
+    sheetShape: Shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
     content: @Composable (PaddingValues) -> Unit,
 ) {
     val scaffoldState = rememberBottomSheetScaffoldState()
 
     BottomSheetScaffold(
-        modifier = modifier,
+        modifier = modifier
+            .safeDrawingPadding(),
         scaffoldState = scaffoldState,
+        sheetContainerColor = MainThemeColor.Gray3,
         sheetContent = {
-            sheetContent()
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(y = 1.dp)
+                    .clip(sheetShape),
+                color = MainThemeColor.White
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 6.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Surface(
+                        modifier = Modifier
+                            .width(40.dp)
+                            .height(4.dp),
+                        shape = RoundedCornerShape(2.dp),
+                        color = MainThemeColor.Gray3
+                    ) {}
+                }
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                ) {
+                    sheetContent()
+                }
+            }
+
         },
-        sheetPeekHeight = sheetPeekHeight.dp,
+        sheetPeekHeight = sheetPeekHeight,
         sheetShape = sheetShape,
-        content =  content,
+        sheetDragHandle = null,
+        content = content,
     )
 }
