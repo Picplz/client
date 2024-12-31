0ad1371a537f6f2e2b28bb90fcfa964372d82304
ldhldh07 <dhlim07@naver.com>
2024-12-31 21:56:42 +0900

    :sparkles: : 더미 작가 리스트 수정

--- app/src/main/kotlin/com/hm/picplz/data/model/Photographer.kt
+++ app/src/main/kotlin/com/hm/picplz/data/model/Photographer.kt
@@ -12,42 +12,42 @@ data class Photographer (
 val dummyPhotographers = listOf(
     Photographer(
         name = "작가1",
-        location = LatLng.from(37.411960, 127.115587),
+        location = LatLng.from(37.420960, 127.115587),
         profileImageUri = "https://picsum.photos/200"
     ),
     Photographer(
         name = "작가2",
-        location = LatLng.from(37.409960, 127.119587),
+        location = LatLng.from(37.412510, 127.125137),
         profileImageUri = "https://picsum.photos/200"
     ),
     Photographer(
         name = "작가3",
-        location = LatLng.from(37.402960, 127.119587),
+        location = LatLng.from(37.402960, 127.124587),
         profileImageUri = "https://picsum.photos/200"
     ),
     Photographer(
         name = "작가4",
-        location = LatLng.from(37.397960, 127.118587),
+        location = LatLng.from(37.392960, 127.125587),
         profileImageUri = "https://picsum.photos/200"
     ),
     Photographer(
         name = "작가5",
-        location = LatLng.from(37.397960, 127.115587),
+        location = LatLng.from(37.384960, 127.115587),
         profileImageUri = "https://picsum.photos/200"
     ),
     Photographer(
         name = "작가6",
-        location = LatLng.from(37.398960, 127.111587),
+        location = LatLng.from(37.392960, 127.105587),
         profileImageUri = "https://picsum.photos/200"
     ),
     Photographer(
         name = "작가7",
-        location = LatLng.from(37.402960, 127.111587),
+        location = LatLng.from(37.402960, 127.106587),
         profileImageUri = "https://picsum.photos/200"
     ),
     Photographer(
         name = "작가8",
-        location = LatLng.from(37.406960, 127.112587),
+        location = LatLng.from(37.412960, 127.105587),
         profileImageUri = "https://picsum.photos/200"
     )
 )
