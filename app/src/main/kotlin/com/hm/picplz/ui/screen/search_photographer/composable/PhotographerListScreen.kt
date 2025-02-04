9f5e49e7892b0578a81eaa62692e2136e86e943c
ldhldh07 <dhlim07@naver.com>
2025-02-04 14:38:49 +0900

    :lipstick: : 태그리스트 구현

--- app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/composable/PhotographerListScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/composable/PhotographerListScreen.kt
@@ -1,15 +1,31 @@
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hm.picplz.R
 import com.hm.picplz.ui.theme.MainThemeColor
+
+data class StatusTagData(
+    val label: String,
+    val iconResId: Int
+)
+
+private val statusTags = listOf(
+    StatusTagData("바로 촬영", R.drawable.tag_circle),
+    StatusTagData("팔로우", R.drawable.tag_check),
+    StatusTagData("바로 촬영 가능", R.drawable.tag_camera),
+)
 
 @Composable
 fun PhotographerListScreen() {
@@ -24,6 +40,21 @@ fun PhotographerListScreen() {
             color = MainThemeColor.Black
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Box(modifier = Modifier.fillMaxWidth().height(400.dp))
+        Row {
+            statusTags.forEach { statusTag ->
+                CommonStatusTag(
+                    label = statusTag.label,
+                    icon = painterResource(id = statusTag.iconResId)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+            }
+        }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun PhotographerListScreenPreview() {
+    PhotographerListScreen()
+}
+
