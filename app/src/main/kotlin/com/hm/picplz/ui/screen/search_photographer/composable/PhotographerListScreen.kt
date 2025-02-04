90b77e4a6c825af04a9c601f9a92b96a1589ddeb
ldhldh07 <dhlim07@naver.com>
2025-02-04 17:39:02 +0900

    :lipstick: : 바이브 태그 리스트 ui 구현

--- app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/composable/PhotographerListScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/composable/PhotographerListScreen.kt
@@ -1,11 +1,15 @@
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +18,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hm.picplz.R
+import com.hm.picplz.data.model.ChipMode
 import com.hm.picplz.ui.theme.MainThemeColor
 
 data class StatusTagData(
@@ -27,6 +32,13 @@ private val statusTags = listOf(
     StatusTagData("바로 촬영 가능", R.drawable.tag_camera),
 )
 
+private val vibeTags = listOf(
+    "#을지로 감성",
+    "#키치 감성",
+    "#MZ 감성",
+    "#퇴폐 감성"
+)
+
 @Composable
 fun PhotographerListScreen() {
     Column(
@@ -34,12 +46,6 @@ fun PhotographerListScreen() {
             .fillMaxWidth()
             .padding(16.dp)
     ) {
-        Text(
-            text = "주변 작가",
-            style = MaterialTheme.typography.titleMedium,
-            color = MainThemeColor.Black
-        )
-        Spacer(modifier = Modifier.height(16.dp))
         Row {
             statusTags.forEach { statusTag ->
                 CommonStatusTag(
@@ -47,6 +53,20 @@ fun PhotographerListScreen() {
                     icon = painterResource(id = statusTag.iconResId)
                 )
                 Spacer(modifier = Modifier.width(4.dp))
+            }
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+        ) {
+            itemsIndexed(vibeTags) { index, vibeTag ->
+                CommonChip(
+                    id = index.toString(),
+                    label = vibeTag,
+                    initialMode = ChipMode.DEFAULT,
+                    isEditable = false,
+                    height = ChipHeight.MEDIUM,
+                )
             }
         }
     }
