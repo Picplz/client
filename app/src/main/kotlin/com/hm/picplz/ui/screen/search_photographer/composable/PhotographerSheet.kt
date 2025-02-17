0bee18f4db1224b8b43791ba330ada6186ed4045
ldhldh07 <dhlim07@naver.com>
2025-02-17 22:51:00 +0900

    :lipstick: : 작가 시트 ui 개발

--- app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/composable/PhotographerSheet.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/search_photographer/composable/PhotographerSheet.kt
@@ -2,17 +2,23 @@ package com.hm.picplz.ui.screen.search_photographer.composable
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -38,7 +44,9 @@ fun PhotographerSheet (
             .fillMaxWidth()
             .padding(top = 12.dp)
     ) {
-        Row() {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
             Image(
                 painter = rememberAsyncImagePainter(model = selectedPhotographer?.profileImageUri),
                 contentDescription = "작가 프로필 이미지",
@@ -57,7 +65,6 @@ fun PhotographerSheet (
                 ),
                 modifier = Modifier
                     .padding(start = 4.dp)
-
             )
             Text(
                 text = selectedPhotographer?.socialAccount ?: "",
@@ -66,8 +73,53 @@ fun PhotographerSheet (
                     fontWeight = FontWeight.Normal,
                     fontSize = 12.sp,
                     letterSpacing = 0.sp
-                )
+                ),
+                modifier = Modifier
+                    .padding(start = 4.dp)
             )
+        }
+        Row(
+            modifier = Modifier
+                .padding(start = 4.dp, top = 10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ){
+            ActiveStatusBadge(text = "바로 촬영")
+            DistanceText(
+                distance = selectedPhotographer?.distance.toString(),
+                duration = "도보 10분 거리",
+                modifier = Modifier
+                    .padding(start = 4.dp)
+            )
+        }
+        val vibeTags = listOf(
+            "#을지로 감성",
+            "#키치 감성",
+            "#MZ 감성",
+            "#퇴폐 감성"
+        )
+        VibeTags(
+            modifier = Modifier.padding(top = 20.dp),
+            tags = vibeTags
+        )
+        LazyRow(
+            modifier = Modifier
+                .padding(top = 10.dp),
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            selectedPhotographer?.portfolioPhotos?.let { photos ->
+                itemsIndexed(photos) { _, photoUrl ->
+                    Image(
+                        painter = rememberAsyncImagePainter(
+                            model = photoUrl,
+                            contentScale = ContentScale.Crop
+                        ),
+                        contentDescription = "포트폴리오 사진",
+                        modifier = Modifier
+                            .size(100.dp)
+                            .clip(RoundedCornerShape(5.dp))
+                    )
+                }
+            }
         }
     }
 }
