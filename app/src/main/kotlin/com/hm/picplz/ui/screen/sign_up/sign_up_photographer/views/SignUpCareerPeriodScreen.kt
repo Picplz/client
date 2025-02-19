a4005917fc0114275213dc6610e99d9edc3f2c1f
ldhldh07 <dhlim07@naver.com>
2025-02-19 18:23:12 +0900

    :lipstick: : 경력 입력 영역 기본 틀 추가

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_photographer/views/SignUpCareerPeriodScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_photographer/views/SignUpCareerPeriodScreen.kt
@@ -1,15 +1,17 @@
 package com.hm.picplz.ui.screen.sign_up.sign_up_photographer.views
 
+import CommonOutlinedTextField
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,18 +20,15 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.hm.picplz.data.model.ChipMode
 import com.hm.picplz.ui.screen.common.CommonBottomButton
 import com.hm.picplz.ui.screen.common.CommonTopBar
-import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.Navigate
 import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.NavigateToPrev
-import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.SetPhotographyExperience
-import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.SetUserPhotographyExperience
 import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerSideEffect
 import com.hm.picplz.ui.theme.MainThemeColor
 import com.hm.picplz.ui.theme.PicplzTheme
@@ -85,8 +84,45 @@ fun SignUpCareerPeriodScreen(
                     )
                     Spacer(
                         modifier = Modifier
-                            .height(30.dp)
+                            .height(30.dp),
                     )
+                    Row(
+                        modifier = Modifier
+                            .height(40.dp),
+                        verticalAlignment = Alignment.Bottom
+                    ) {
+                        CommonOutlinedTextField(
+                            modifier = Modifier.width(85.dp),
+                            value = "0",
+                            onValueChange = {},
+                            placeholder = "0",
+                            imeAction = ImeAction.Next,
+                            keyboardActions = {},
+                            readOnly = true,
+                            showError = false,
+                        )
+                        Spacer(modifier = modifier.width(7.dp))
+                        Text(
+                            text = "년",
+                            style = MaterialTheme.typography.titleMedium,
+                        )
+                        Spacer(modifier = modifier.width(20.dp))
+                        CommonOutlinedTextField(
+                            modifier = Modifier.width(85.dp),
+                            value = "0",
+                            onValueChange = {},
+                            placeholder = "0",
+                            imeAction = ImeAction.Next,
+                            keyboardActions = {},
+                            readOnly = true,
+                            showError = false,
+                        )
+                        Spacer(modifier = modifier.width(7.dp))
+                        Text(
+                            text = "개월",
+                            style = MaterialTheme.typography.titleMedium,
+                        )
+                    }
                 }
             }
             Box(
