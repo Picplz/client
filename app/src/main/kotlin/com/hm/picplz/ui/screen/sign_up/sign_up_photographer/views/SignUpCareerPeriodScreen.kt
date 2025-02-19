2b73589a33181a99017b377b897d9e8976c4422f
ldhldh07 <dhlim07@naver.com>
2025-02-19 17:32:02 +0900

    :lipstick: : 경력 기간 화면 ui 개발 - todo : 입력창

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_photographer/views/SignUpCareerPeriodScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_photographer/views/SignUpCareerPeriodScreen.kt
@@ -1,19 +1,35 @@
 package com.hm.picplz.ui.screen.sign_up.sign_up_photographer.views
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.hm.picplz.data.model.ChipMode
+import com.hm.picplz.ui.screen.common.CommonBottomButton
 import com.hm.picplz.ui.screen.common.CommonTopBar
+import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.Navigate
 import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.NavigateToPrev
+import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.SetPhotographyExperience
+import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.SetUserPhotographyExperience
 import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerSideEffect
 import com.hm.picplz.ui.theme.MainThemeColor
 import com.hm.picplz.ui.theme.PicplzTheme
@@ -41,6 +57,51 @@ fun SignUpCareerPeriodScreen(
                 text = "경력 선택",
                 onClickBack = {viewModel.handleIntent(NavigateToPrev)}
             )
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 15.dp)
+                    .imePadding(),
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Spacer(
+                        modifier = Modifier
+                            .height(80.dp)
+                    )
+                    Text(
+                        text = "경력 기간을 입력해주세요.",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .height(15.dp)
+                    )
+                    Text(
+                        text = "1년 미만일 경우 0년 n개월로 입력해주세요.",
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .height(30.dp)
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .height(120.dp)
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                CommonBottomButton(
+                    text = "다음",
+                    onClick = {},
+                    containerColor = MainThemeColor.Black
+                )
+            }
         }
     }
 
