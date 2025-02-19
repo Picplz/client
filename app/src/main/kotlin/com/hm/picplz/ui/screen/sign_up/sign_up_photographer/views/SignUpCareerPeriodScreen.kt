9f4614d66e6506c05bab47397b705fc2147a6bb9
ldhldh07 <dhlim07@naver.com>
2025-02-19 17:10:25 +0900

    :lipstick: : 경력 기간 스크린 기본 레이아웃 설정

---
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_photographer/views/SignUpCareerPeriodScreen.kt
@@ -0,0 +1,54 @@
+package com.hm.picplz.ui.screen.sign_up.sign_up_photographer.views
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.hm.picplz.ui.screen.common.CommonTopBar
+import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.NavigateToPrev
+import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerSideEffect
+import com.hm.picplz.ui.theme.MainThemeColor
+import com.hm.picplz.viewmodel.SignUpPhotographerViewModel
+import kotlinx.coroutines.flow.collectLatest
+
+@Composable
+fun SignUpCareerPeriodScreen(
+    modifier: Modifier = Modifier,
+    viewModel: SignUpPhotographerViewModel = viewModel(),
+    signUpPhotographerNavController: NavController
+) {
+    val currentState = viewModel.state.collectAsState().value
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        containerColor = MainThemeColor.White
+    ) {innerPadding ->
+        Column(
+            modifier = modifier
+                .padding(innerPadding)
+        ) {
+            CommonTopBar(
+                text = "경력 선택",
+                onClickBack = {viewModel.handleIntent(NavigateToPrev)}
+            )
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.sideEffect.collectLatest { sideEffect ->
+            when (sideEffect) {
+                is SignUpPhotographerSideEffect.NavigateToPrev -> {
+                    signUpPhotographerNavController.popBackStack()
+                }
+                else -> {}
+            }
+        }
+    }
+}
